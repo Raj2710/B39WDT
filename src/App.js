729d16c30cf6e948eea051debe90a21214d9bee2
Nagarajan <nagarajanc@theecode.xyz>
2022-09-27 11:44:40 +0530

    Price Card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import PriceCard from './components/PriceCard';
+
+let data = [
+  {
+    plan:"FREE",
+    price:"0",
+    users:"Single User",
+    usersEnabler:true,
+    storage:"5GB Storage",
+    storageEnabler:true,
+    publicProjects:"Unlimited Public Projects",
+    publicProjectsEnabler:true,
+    communityAccess:"Community Access",
+    communityAccessEnabler:true,
+    privateProjects:"Unlimited Private Projects",
+    privateProjectsEnabler:false,
+    phoneSupport:"Dedicated Phone Support",
+    phoneSupportEnabler:false,
+    subDomain:"Free Subdomain",
+    subDomainEnabler:false,
+    statusReports:"Monthly Status Reports",
+    statusReportsEnabler:false
+  },
+  {
+    plan:"Plus",
+    price:"9",
+    users:"5 User",
+    usersEnabler:true,
+    storage:"50GB Storage",
+    storageEnabler:true,
+    publicProjects:"Unlimited Public Projects",
+    publicProjectsEnabler:true,
+    communityAccess:"Community Access",
+    communityAccessEnabler:true,
+    privateProjects:"Unlimited Private Projects",
+    privateProjectsEnabler:true,
+    phoneSupport:"Dedicated Phone Support",
+    phoneSupportEnabler:true,
+    subDomain:"Free Subdomain",
+    subDomainEnabler:true,
+    statusReports:"Monthly Status Reports",
+    statusReportsEnabler:false
+  },
+  {
+    plan:"PRO",
+    price:"49",
+    users:"Unlimited Users",
+    usersEnabler:true,
+    storage:"150GB Storage",
+    storageEnabler:true,
+    publicProjects:"Unlimited Public Projects",
+    publicProjectsEnabler:true,
+    communityAccess:"Community Access",
+    communityAccessEnabler:true,
+    privateProjects:"Unlimited Private Projects",
+    privateProjectsEnabler:true,
+    phoneSupport:"Dedicated Phone Support",
+    phoneSupportEnabler:true,
+    subDomain:"Unlimited Free Subdomains",
+    subDomainEnabler:true,
+    statusReports:"Monthly Status Reports",
+    statusReportsEnabler:true
+  }
+]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <>
+    <section className="pricing py-5">
+    <div className="container">
+        <div className="row">
+          <PriceCard data={data[0]}/>
+          <PriceCard data={data[1]}/>
+          <PriceCard data={data[2]}/>
+        </div>
     </div>
-  );
+    </section>
+  </>
 }
 
 export default App;
